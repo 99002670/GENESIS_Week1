c0b7407c4f1fc382a4d4d4d00f8314070625dc5e
Milind Mohapatra <milind.mohapatra@ltts.com>
2020-09-18 23:58:25 +0530

    Initial commit

---
+++ src/neon.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int neon(int number)
+{
+	int sum = 0;
+	int neonFlag = 0;
+	int square = number * number;
+	while(square > 0)
+	{
+		int digit = square % 10;
+		sum += digit;
+		square /= 10;
+	}
+	if(sum == number)
+	{
+		neonFlag = 1;
+	}
+}
+
